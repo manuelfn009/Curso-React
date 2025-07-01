6576b4564e1aaf657e6b10b4c707fb8abffc737a
Manuel Fernández <manuelfn066@gmail.com>
2025-07-01 18:30:06 +0200

    Update App.jsx

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,14 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { use, useState } from "react";
+
+const TURNS = {
+  X: "X",
+  O: "O",
+};
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? "is-selected" : ""}`;
+  return <div onClick={updateBoard} className={className}>{children}</div>;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.X);
 
   return (
-    <h1>tic tac toe</h1>
-  )
+    <main className="board">
+      <h1>Tic Tac Toe</h1>
+      <section className="game">
+        {board.map((cell, index) => (
+          <Square 
+          key={index} 
+          index={index}
+          updateBoard={updateBoard}
+          >
+            {cell}
+          </Square>
+        ))}
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
