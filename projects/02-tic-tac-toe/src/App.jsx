84d271a38cdd61ba533f0a46dd360f7d936f4b59
Manuel Fernández <manuelfn066@gmail.com>
2025-07-02 13:38:31 +0200

    Update Project 2

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -7,23 +7,77 @@ const TURNS = {
 
 const Square = ({ children, isSelected, updateBoard, index }) => {
   const className = `square ${isSelected ? "is-selected" : ""}`;
-  return <div onClick={updateBoard} className={className}>{children}</div>;
+
+  const handleClick = () => {
+    updateBoard(index);
+  };
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  );
 };
+
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
+  const [winner, setWinner] = useState(null); // null significa que no hay ganador aún, false significa que hay un empate
+
+  const checkWinner = (boardToCheck) => {
+    // revisar si hay un ganador
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]; // retorna el ganador
+      }
+    }
+    // si no hay ganador, revisar si hay un empate
+    if (board.every((cell) => cell !== null)) {
+      return false; // empate
+    }
+    return null; // aún no hay ganador ni empate
+  };
+
+  const updateBoard = (index) => {
+    // no actualizar el tablero si ya hay un ganador
+    if (winner) return;
+    // si el index es inválido o la celda ya está ocupada, no hacer nada
+    if (board[index]) return;
+    // actualizar el tablero
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+    // cambiar el turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+    // revisar si hay un ganador
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(()=> {return newWinner});
+    }
+  };
 
   return (
     <main className="board">
       <h1>Tic Tac Toe</h1>
       <section className="game">
         {board.map((cell, index) => (
-          <Square 
-          key={index} 
-          index={index}
-          updateBoard={updateBoard}
-          >
+          <Square key={index} index={index} updateBoard={updateBoard}>
             {cell}
           </Square>
         ))}
